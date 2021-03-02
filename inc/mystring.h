d86b89c39e80e868687b4a73e154ec232b5f8321
99003585 <theresmary98.com>
2021-03-02 09:38:42 +0530

    header files

--- inc/mystring.h
+++ inc/mystring.h
@@ -1,8 +1,11 @@
 #include<string.h>
+
 #ifndef __MYSTRING_H
 #define __MYSTRING_H
-size_t mystrlen(const char *str);
-char* mystrcpy(char* dest, const char* src);
-char* mystrcat(char *destination, const char *source);
-int mystrcmp(const char *leftStr, const char *rightStr );
+
+int mystrlen(const char *str);
+char* mystrcpy(char* String1, const char* String2);
+char* mystrcat(char *String1, const char *String2);
+int mystrcmp(const char *String1, const char *String2);
+
 #endif
--- inc/number.h
+++ inc/number.h
@@ -1,6 +1,8 @@
 #ifndef __NUMBER_H
 #define __NUMBER_H
+
 int factorial(int);
 int isPrime(int a);
 int ispalindrome(int n);
+
 #endif
